fd99cb7e264effe1599d06aa8efef222ced30397
T <www1532621280@qq.com>
2025-03-30 21:42:01 +0800

    补回配置信息

---
+++ src/.vuepress/navbar.ts
@@ -0,0 +1,32 @@
+import { navbar } from "vuepress-theme-hope";
+
+export default navbar([
+  "/",
+  {
+    text: "设计模式",
+    icon: "check",
+    link: "/design-patterns/"
+    
+  },
+  {
+    text: "后端主航",
+    icon: "pen-to-square",
+    link: "/backend/ssm/"
+    
+  },
+  {
+    text: "前端掌舵",
+    icon: "face-smile",
+    link: "/frontend/",
+  },
+  {
+    text: "读书笔记",
+    icon: "book",
+    link: "/reading/",
+  },
+  {
+    text: "周记",
+    icon: "pen-to-square",
+    link: "/weekly-diary/",
+  }
+]);
